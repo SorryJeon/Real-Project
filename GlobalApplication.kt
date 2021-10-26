df0a9580ab6c0ce73464a6bd86a58fa80f147e1a
Jeon-Jae-Hyoung <developerjeon22@gmail.com>
2021-10-26 19:21:17 +0900

    참고자료 업로드!

---
+++ GlobalApplication.kt
@@ -0,0 +1,22 @@
+class GlobalApplication : Application() {
+    	override fun onCreate() {
+        	super.onCreate()
+
+        	instance = this
+        	KakaoSDK.init(KakaoSDKAdapter())
+    	}
+
+        override fun onTerminate() {
+            super.onTerminate()
+            instance = null
+        }
+
+        fun getGlobalApplicationContext(): GlobalApplication {
+            checkNotNull(instance) { "this application does not inherit com.kakao.GlobalApplication" }
+            return instance!!
+        }
+
+        companion object {
+            var instance: GlobalApplication? = null
+        }
+	}
