276c1d8e156ab57a791c0e5449baa0542c79407c
Jeon-Jae-Hyoung <developerjeon22@gmail.com>
2022-05-21 15:40:02 +0900

    feat : SettingFragment 로그아웃 & 회원탈퇴 기능 구현

--- FakeCarrotMarket/app/src/main/java/com/example/app/fakecarrotmarket/Fragment/SettingFragment.kt
+++ FakeCarrotMarket/app/src/main/java/com/example/app/fakecarrotmarket/Fragment/SettingFragment.kt
@@ -1,6 +1,9 @@
 package com.example.app.fakecarrotmarket
 
+import android.annotation.SuppressLint
 import android.content.ContentValues.TAG
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import androidx.fragment.app.Fragment
@@ -9,19 +12,35 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import com.bumptech.glide.Glide
+import com.example.app.fakecarrotmarket.databinding.FragmentSettingBinding
+import com.facebook.login.LoginManager
 import com.google.android.gms.auth.api.signin.GoogleSignIn
 import com.google.android.gms.auth.api.signin.GoogleSignInClient
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.tasks.OnCompleteListener
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.messaging.FirebaseMessaging
+import com.kakao.auth.Session
+import com.twitter.sdk.android.core.TwitterCore
 
 class SettingFragment : Fragment() {
 
     private lateinit var googleSignInClient: GoogleSignInClient
+    var fbFireStore: FirebaseFirestore? = null
+    private var binding: FragmentSettingBinding? = null
     private var temp: String? = null
     private var inputId: TextView? = null
     private var inputName: TextView? = null
     private var iv: ImageView? = null
+
+    private var firebaseDatabase = FirebaseDatabase.getInstance()
+    private var databaseReference = firebaseDatabase.reference
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -43,6 +62,7 @@ class SettingFragment : Fragment() {
 
     }
 
+    @SuppressLint("SetTextI18n")
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -70,6 +90,38 @@ class SettingFragment : Fragment() {
         return view
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val fragmentSettingBinding = FragmentSettingBinding.bind(view)
+        binding = fragmentSettingBinding
+
+        binding!!.btnLogout2.setOnClickListener {
+            signOut()
+            Log.d(TAG, "고구마켓이 종료되었습니다.")
+        }
+
+        binding!!.btnRevoke2.setOnClickListener {
+            revokeAccess()
+            Log.d(TAG, "고구마켓이 종료되었습니다.")
+        }
+
+        binding!!.btnToken2.setOnClickListener {
+            FirebaseMessaging.getInstance().token.addOnCompleteListener(OnCompleteListener { task ->
+                if (!task.isSuccessful) {
+                    Log.w(TAG, "Fetching FCM registration token failed", task.exception)
+                    return@OnCompleteListener
+                }
+
+                val token = task.result
+
+                val msg = getString(R.string.msg_token_fmt, token)
+                Log.d(TAG, msg)
+                Toast.makeText(requireContext(), msg, Toast.LENGTH_SHORT).show()
+            })
+        }
+    }
+
     override fun onStart() {
         // 어플을 실행할 때 마다 Logcat 시스템으로 알려줌
         super.onStart()
@@ -79,5 +131,84 @@ class SettingFragment : Fragment() {
 
     override fun onResume() {
         super.onResume()
+    }
+
+    private fun signOut() {
+        val account = GoogleSignIn.getLastSignedInAccount(requireContext())
+        val inflater =
+            requireActivity().getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
+        val view = inflater.inflate(R.layout.alert_popup, null)
+        val textView: TextView = view.findViewById(R.id.textView)
+        textView.text = "로그아웃을 하시겠습니까?"
+        val alertdialog = AlertDialog.Builder(requireContext())
+            .setTitle("로그아웃 페이지")
+            .setPositiveButton("예") { dialog, which ->
+
+                TwitterCore.getInstance().sessionManager.clearActiveSession()
+                FirebaseAuth.getInstance().signOut()
+                LoginManager.getInstance().logOut()
+                Session.getCurrentSession().close()
+                if (account !== null) {
+                    googleSignInClient.signOut().addOnCompleteListener(requireActivity()) {
+                    }
+                }
+                Toast.makeText(requireContext(), "로그아웃이 완료되었습니다.", Toast.LENGTH_SHORT)
+                    .show()
+                Log.d(TAG, "로그아웃이 완료되었습니다.")
+                val intent = Intent(activity, LoginActivity::class.java)
+                startActivity(intent)
+                activity?.supportFragmentManager
+                    ?.beginTransaction()
+                    ?.remove(this)
+                    ?.commit()
+                Log.d(TAG, "로그인 화면으로 이동합니다.")
+            }
+            .setNegativeButton("아니오", null)
+            .create()
+
+        alertdialog.setView(view)
+        alertdialog.show()
+    }
+
+    private fun revokeAccess() {
+        val account = GoogleSignIn.getLastSignedInAccount(requireContext())
+        val inflater =
+            requireActivity().getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
+        val view = inflater.inflate(R.layout.alert_popup, null)
+        val textView: TextView = view.findViewById(R.id.textView)
+        textView.text = "회원탈퇴를 하시겠습니까?"
+        val alertdialog = AlertDialog.Builder(requireContext())
+            .setTitle("회원탈퇴 페이지")
+            .setPositiveButton("Yes") { dialog, which ->
+
+                TwitterCore.getInstance().sessionManager.clearActiveSession()
+                if (auth!!.currentUser != null) {
+                    auth!!.currentUser!!.delete()
+                }
+
+                fbFireStore?.collection("users")?.document(auth?.uid.toString())?.delete()
+                databaseReference.child("Users").child(auth?.currentUser!!.uid).removeValue()
+                FirebaseAuth.getInstance().signOut()
+                LoginManager.getInstance().logOut()
+                Session.getCurrentSession().close()
+
+                if (account !== null) {
+                    googleSignInClient.revokeAccess().addOnCompleteListener(requireActivity()) {
+                    }
+                }
+                Toast.makeText(requireContext(), "회원탈퇴가 완료되었습니다.", Toast.LENGTH_SHORT)
+                    .show()
+                Log.d(TAG, "회원탈퇴가 완료되었습니다.")
+                val intent = Intent(activity, LoginActivity::class.java)
+                startActivity(intent)
+
+                Log.d(TAG, "로그인 화면으로 이동합니다.")
+            }
+
+            .setNegativeButton("No", null)
+            .create()
+
+        alertdialog.setView(view)
+        alertdialog.show()
     }
 }
