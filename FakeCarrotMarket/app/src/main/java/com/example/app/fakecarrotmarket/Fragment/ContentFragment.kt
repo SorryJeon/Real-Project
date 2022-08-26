595eb74962e71f8ec704f974b859449a498c4d93
Jeon-Jae-Hyoung <developerjeon22@gmail.com>
2022-08-26 23:38:06 +0900

    style : Gogumarket 디자인 전면 개편 (1)

--- FakeCarrotMarket/app/src/main/java/com/example/app/fakecarrotmarket/Fragment/ContentFragment.kt
+++ FakeCarrotMarket/app/src/main/java/com/example/app/fakecarrotmarket/Fragment/ContentFragment.kt
@@ -2,7 +2,6 @@ package com.example.app.fakecarrotmarket
 
 import android.annotation.SuppressLint
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -11,18 +10,11 @@ import com.example.app.fakecarrotmarket.databinding.FragmentContentBinding
 import com.google.android.gms.auth.api.signin.GoogleSignIn
 import com.google.android.gms.auth.api.signin.GoogleSignInClient
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions
-import com.google.android.youtube.player.YouTubeInitializationResult
-import com.google.android.youtube.player.YouTubePlayer
-import kr.co.prnd.YouTubePlayerView
 
 class ContentFragment : Fragment() {
 
     private lateinit var googleSignInClient: GoogleSignInClient
     private var binding: FragmentContentBinding? = null
-    private val videoId = "hl-ii7W4ITg"
-
-//    private var firebaseDatabase = FirebaseDatabase.getInstance()
-//    private var databaseReference = firebaseDatabase.reference
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -58,21 +50,4 @@ class ContentFragment : Fragment() {
         val fragmentContentBinding = FragmentContentBinding.bind(view)
         binding = fragmentContentBinding
     }
-
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
-
-        val youtubePlayerView = view?.findViewById<YouTubePlayerView>(R.id.youtubePlayerView)
-        youtubePlayerView?.play(videoId)
-    }
-
-//    private fun playVideo() {
-//        if (player != null) {
-//            if (player!!.isPlaying) {
-//                player?.pause()
-//            }
-//            player?.cueVideo(videoId)
-//        }
-//    }
-
 }
